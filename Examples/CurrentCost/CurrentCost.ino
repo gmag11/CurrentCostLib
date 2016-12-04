55a1bb24875c1953cb28f5d1f7578aa3731c6c23
Germán Martín <gmag11@hotmail.com>
2016-12-04 08:18:51 +0100

    Complete example

--- Examples/CurrentCost/CurrentCost.ino
+++ Examples/CurrentCost/CurrentCost.ino
@@ -1,19 +1,31 @@
-//#include "Sensor.h"
 #include <ArduinoJson.h>
 #include <Ticker.h>
 #include "CurrentCostLib.h"
+
+#define DEBUG
+
+#ifdef DEBUG
+#define DEBUGLOG(...) DBG_OUTPUT_PORT.printf(__VA_ARGS__)
+#else
+#define DEBUGLOG(...)
+#endif
+
+void processSensorEvent(uint8_t id, int watts, float tempr);
 
 void setup()
 {
 	Serial.begin(115200);
 	currentCost.begin(Serial);
-  /* add setup code here */
-
+	currentCost.onSensorEvent(processSensorEvent);
 }
 
 void loop()
 {
-	
-  /* add main program code here */
+	currentCost.handle();
+}
 
+void processSensorEvent(uint8_t id, int watts, float tempr) {
+	DEBUGLOG("---->  Event %d\n", id);
+	DEBUGLOG("Power: %d W\n", watts);
+	DEBUGLOG("Temp: %.2f C\n\n", tempr);
 }
